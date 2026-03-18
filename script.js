08367d2533dd5d6fab5957b14cba9525e75c7092
aashish14pd <aashishpp2002@gmail.com>
2026-03-18 14:59:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
-document.querySelectorAll('nav a').forEach(anchor => {
+// DARK MODE TOGGLE
 
-anchor.addEventListener('click', function(e){
+const toggleBtn = document.getElementById("theme-toggle");
 
-e.preventDefault();
+toggleBtn.addEventListener("click", () => {
 
-document.querySelector(this.getAttribute('href'))
-.scrollIntoView({
-behavior: 'smooth'
+document.body.classList.toggle("dark-mode");
+
+if(document.body.classList.contains("dark-mode")){
+toggleBtn.innerText="☀️";
+}else{
+toggleBtn.innerText="🌙";
+}
+
 });
 
+
+// SCROLL ANIMATION
+
+const cards = document.querySelectorAll(".flow-card");
+
+const observer = new IntersectionObserver(entries=>{
+entries.forEach(entry=>{
+if(entry.isIntersecting){
+entry.target.style.opacity="1";
+entry.target.style.transform="translateY(0)";
+}
+});
 });
 
+cards.forEach(card=>{
+card.style.opacity="0";
+card.style.transform="translateY(40px)";
+card.style.transition="0.6s";
+observer.observe(card);
 });
